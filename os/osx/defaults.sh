279b57f4152012a665f4c37acb67dec9e316cac3
david <stuff@thefosters.com>
2015-05-13 23:13:12 -0700

    removed killall section

--- os/osx/defaults.sh
+++ os/osx/defaults.sh
@@ -147,14 +147,4 @@ echo ""
 echo "Rebuild the index from scratch"
 sudo mdutil -E / > /dev/null
 
-###############################################################################
-# Kill affected applications
-###############################################################################
-
-for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
-	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-	"Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
-	"Transmission" "Twitter" "iCal"; do
-		echo "Affected app ${app}"
-done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
