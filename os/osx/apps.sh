e6bd718a58590f2b79b23f434bfc48ce0639db34
david <stuff@thefosters.com>
2015-05-13 19:57:43 -0700

    fixed brew cask installation

--- os/osx/apps.sh
+++ os/osx/apps.sh
@@ -113,8 +113,7 @@ main() {
 
   # Install homebrew-cask
   echo "installing cask..."
-  brew tap phinze/homebrew-cask
-  brew install brew-cask
+  brew install caskroom/cask/brew-cask
 
   # Tap alternative versions
   brew tap caskroom/versions
